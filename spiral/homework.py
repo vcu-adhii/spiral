bc784ee61da972d47db39961a94141c578c525e8
Ibadadhi <72210975+Ibadadhi@users.noreply.github.com>
2021-04-07 16:49:53 -0400

    Update homework.py

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,5 +1,5 @@
 def spiralize(num):
-    var2 = (num - 1) / 2
+    var22 = (num - 1) / 2
     var1 = (3 + 2 * var2 * (8 * var2 * var2 + 15 * var2 + 13)) / 3
     return_value = var1
     return return_value
