2f46784847a6e599b6b132f75414d1191e9e9860
Ibadadhi <72210975+Ibadadhi@users.noreply.github.com>
2021-04-08 16:44:38 -0400

    Update homework.py

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,5 +1,17 @@
-def spiralize(num):
-    var22 = (num - 1) / 2
-    var1 = (3 + 2 * var2 * (8 * var2 * var2 + 15 * var2 + 13)) / 3
-    return_value = var1
-    return return_value
+def spiralize(number):
+    x = 1
+    y = 2
+    z = 0
+    prev_step = 0
+
+    while x <= number ** 2:
+        z += x
+        x += y
+        prev_step += 1
+        if prev_step == 4:
+            y += 2
+            prev_step = 0
+    return total
+
+
+print(spiralize(501))
